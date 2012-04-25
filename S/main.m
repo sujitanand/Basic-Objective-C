37d31aae5e6a23875fed96e9860b8fddb287aa3d
sujit anand karkun <skarkun@dcmac13.dc.engr.scu.edu>
2012-04-24 19:09:35 -0700

    Initial Commit

---
+++ S/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  S
+//
+//  Created by sujit anand karkun on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
